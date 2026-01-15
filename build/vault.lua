a91c2be3b0c89766df407250fbb7ac1b08b3becc
Rani Elhusseini <charmful0x@gmail.com>
2026-01-15 22:45:40 +0100

    feat: initial types

--- build/vault.lua
+++ build/vault.lua
@@ -1,2 +1,25 @@
 local bint = require(".bint")(256)
 local json = require("json")
+
+Balance = {}
+
+Variant = "0.1.0"
+Name = Name or nil
+Identifer = Identifer or nil
+
+TokenReq = {}
+
+
+
+
+
+OrderBookReq = {}
+
+
+
+
+
+SupportedTokens = SupportedTokens or {}
+OrderBooks = OrderBooks or {}
+
+Balances = Balances or {}
